67d701c561692ba92fd8d6d23db1f746cf545d12
Semih Kesgin <SemihK@users.noreply.github.com>
2023-07-21 16:56:52 +0300

    several settings optimized

--- StandByTime/standbytime/ContentView.swift
+++ StandByTime/standbytime/ContentView.swift
@@ -8,23 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
- 
-    var body: some View {
     
-        ZStack {
-            Color.yellow
-                .ignoresSafeArea()
+    let allColors = [Color.black, Color.green.opacity(0.5), Color.yellow, Color.cyan, Color.init(.darkGray), Color.init(.systemGray2)]
+    
+    @State var color = Color.yellow
+    
+var body: some View {
+    ZStack {
+        ColorBackGround(color: $color)
+        VStack {
             Text(Date(), style: .time)
                 .foregroundColor(Color.white)
-                .font(.system(size: 140))
-        .bold()
+                .font(.system(size: 200))
+            .bold()
+            .padding()
+            
+            Button(action: {
+                color = allColors[Int.random(in: 0...5)]
+             }) {
+                 Image(systemName: "lightbulb.circle.fill")
+                     .imageScale(.large)
+             }
+             .foregroundColor(.secondary)
+             .buttonStyle(.borderedProminent)
+                     .buttonBorderShape(.capsule)
+                     .opacity(0.5)
+                  
+                     
         }
+        .ignoresSafeArea()
     }
-    
+}
 }
 
+
+struct ColorBackGround: View {
+@Binding var color: Color
+var body: some View {
+    Rectangle()
+        .fill(color)
+}
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
+/*  ZStack {
+Color.yellow
+    .ignoresSafeArea()
+Text(Date(), style: .time)
+    .foregroundColor(Color.white)
+    .font(.system(size: 140))
+.bold()
+ } /**/*/
