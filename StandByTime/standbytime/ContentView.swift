e352b047aab6c3c9d500578defb7aeca5124f179
Semih Kesgin <SemihK@users.noreply.github.com>
2023-07-27 01:38:22 +0300

    bugfix

--- StandByTime/standbytime/ContentView.swift
+++ StandByTime/standbytime/ContentView.swift
@@ -9,66 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let allColors = [Color.black, Color.green.opacity(0.5), Color.yellow, Color.cyan, Color.init(.darkGray)]
+    @Environment(\.colorScheme) var colorScheme
     
-    @State var color = Color.black
-    
-var body: some View {
-    ZStack {
-        ColorBackGround(color: $color)
+    @State private var currentTime = Date()
+
+    var body: some View {
+        ZStack{
+            Color.black
+                .ignoresSafeArea()
         VStack {
-            Text(Date().formatted(date: .omitted, time: .shortened)) // Çalışmıyor farkındayım neden olduğunu biliyorsan düzelt lütfen. 
-                .tracking(-22)
-                .foregroundStyle(
-                        LinearGradient(
-                            colors: [.blue, .purple, .red],
-                            startPoint: .leading,
-                            endPoint: .trailing
-                        )
-                        
-                    )
+            Text(getFormattedTime())
+                .tracking(-2)
                 .font(.custom(
-                    "Viga-Regular",
-                    fixedSize: 250
-                    
-))
-                .padding(-50)
-                
-            Button(action: {
-                color = allColors[Int.random(in: 0...4)]
-             }) {
-                 Image(systemName: "lightbulb.circle.fill")
-                     .imageScale(.large)
-             }
-             .foregroundColor(.secondary)
-             .buttonStyle(.borderedProminent)
-                    .buttonBorderShape(.capsule)
-                     .opacity(0.5)
+                                    "Viga-Regular",
+                                    fixedSize: 180
+                                    ))
+                .foregroundStyle(
+                                        LinearGradient(
+                                            colors: [.blue, .purple, .red],
+                                            startPoint: .leading,
+                                            endPoint: .trailing
+                                        )
+                                        
+                                    )
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(colorScheme == .dark ? Color.customDarkModeColor : Color.customLightModeColor)
+                        .foregroundColor(colorScheme == .dark ? .white : .black)
         }
-    }   .ignoresSafeArea()
-}
+        .onAppear {
+            // Timer kullanarak saati güncelle
+            let timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                currentTime = Date()
+            }
+            // Timer'ın çalışmasını sağlamak için run loop
+            RunLoop.current.add(timer, forMode: .common)
+        }
+        }
+    }
+
+    // Saati HH:mm:ss formatında almak için yardımcı fonksiyon
+    func getFormattedTime() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss"
+        return formatter.string(from: currentTime)
+    }
 }
 
-
-struct ColorBackGround: View {
-@Binding var color: Color
-var body: some View {
-    Rectangle()
-        .fill(color)
-}
-}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
-
-
-/*  ZStack {
-Color.yellow
-    .ignoresSafeArea()
-Text(Date(), style: .time)
-    .foregroundColor(Color.white)
-    .font(.system(size: 140))
-.bold()
- } /**/*/
---
+++ standbytime/standbytime/Model/darkmode.swift
@@ -0,0 +1,15 @@
+//
+//  darkmode.swift
+//  standbytime
+//
+//  Created by Semih Kesgin on 27.07.2023.
+//
+
+import Foundation
+
+import SwiftUI
+
+public extension Color {
+    static let customDarkModeColor = Color.black
+    static let customLightModeColor = Color.white
+}
